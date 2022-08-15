81336e8b9e8a7fc6fa99026651f94476e8364eb9
Yokto13 <nikousf@seznam.cz>
2022-08-15 11:33:40 +0200

    move things from reg to note, emphasize generic code

--- node.jl
+++ node.jl
@@ -1,16 +1,240 @@
 include("utils.jl")
 
+abstract type Node end
 
-mutable struct Node 
-    is_leaf::Bool
+mutable struct RegNode <: Node
+    isleaf::Bool
     pred::Numeric64
-    examples_indices::Vector{Int64}
-    split_by::Int64
-    value::Numeric64
+    datainds::Vector{Int64}
+    splitval::Numeric64
+    ftr::Integer
     left::Union{Nothing, Node}
     right::Union{Nothing, Node}
 end
 
-Node(v::Vector{Int64}) = Node(false, 0, v, -1, -1, nothing, 
-                              nothing)
-Node() = Node(false, 0, [], -1, -1, nothing, nothing)
+RegNode(v::Vector{Int64}) = RegNode(false, -1, v, -1, -1, nothing, nothing)
+RegNode() = RegNode(false, -1, [], -1, -1, nothing, nothing)
+
+mutable struct ClsNode <: Node
+    isleaf::Bool
+    pred::Numeric64
+    datainds::Vector{Int64}
+    splitval::Numeric64
+    ftr::Integer
+    left::Union{Nothing, Node}
+    right::Union{Nothing, Node}
+end
+
+ClsNode(v::Vector{Int64}) = ClsNode(false, -1, v, -1, -1, nothing, nothing)
+ClsNode() = ClsNode(false, -1, [], -1, -1, nothing, nothing)
+
+mutable struct RegTree
+    data::Vector{Dato}
+    root::RegNode
+end
+
+RegTree(data::Vector{Dato}) = RegTree(data, RegNode())
+
+"""
+    setprediction!(n, data)
+
+Set prediction to node `n` based on `n.datainds`
+"""
+function setprediction!(n::RegNode, data)
+    result::Float64 = 0.0
+    sz = length(n.datainds)
+    for j = 1:sz
+        i = n.datainds[j]
+        result += data[i].y
+    end
+    result/= sz
+    n.pred = result
+end
+
+"""
+    stopdividing(n[, cond])
+
+Determine if node dividing should halt.
+
+If `cond` is unspecified, apply x -> length(x.datainds) < 2.
+"""
+function stopdividing(n::Node, cond::Function= x -> length(x.datainds) < 2)
+    if cond(n)
+        return true
+    end
+    return false
+end
+
+"""
+    evaluate(left, right)
+
+Calculate error for left and right and get its sum.
+
+Used to score how good a certain node split is.
+"""
+function evaluate(left::RegNode, right::RegNode)
+    return MSE(left) + MSE(right)
+end
+
+function MSE(n::Node)
+    sz = length(n.datainds)
+    res::Float64 = 0.0
+    for j = 1:sz
+        i = n.datainds[j]
+        res += (n.pred - data[i].y)^2
+    end
+    res /= sz
+    return res
+end
+
+"""
+    splitnode(ftr, val, node)
+
+Get child-nodes containing `datainds` split on feature `ftr` and value `val`.
+
+All points < `val` are placed to the `left`, the rest to the `right`. 
+"""
+function splitnode(ftr, val, n::Node)
+    left, right = typeof(n)(), typeof(n)()
+    for j = 1:length(n.datainds)
+        if data[n.datainds[j]][ftr] < val
+            push!(left.datainds, n.datainds[j])
+        else
+            push!(right.datainds, n.datainds[j])
+        end
+    end
+    return (left, right)
+end
+
+"""
+    eva;iatesplit(ftr, val, n, data)
+"""
+function evaluatesplit(ftr::Int64, val::Float64, n::Node, data)
+    left, right = splitnode(ftr, val, n)
+    setprediction!(left, data)
+    setprediction!(right, data)
+    return evaluate(left, right)
+end
+
+"""
+    getextremas(v)
+
+Find min and max value in `v`.
+"""
+function getextremas(v::Vector)
+    mi::Float64 = Inf
+    ma::Float64 = -Inf
+    for x = v
+        mi = min(mi, x)
+        ma = max(ma, x)
+    end
+    return (mi, ma)
+end
+
+"""
+    getextremas(feature, n, data)
+
+Find min and max of `ftr` in `n.datainds`.
+"""
+function getextremas(ftr::Int64, n::Node, data)
+    mi::Float64 = Inf
+    ma::Float64 = -Inf
+    for j = 1:length(n.datainds)
+        ex_i = n.datainds[j]
+        mi = min(mi, data[ex_i][ftr])
+        ma = max(ma, data[ex_i][ftr])
+    end
+    @assert(mi != Inf)
+    @assert(ma != -Inf)
+    return (mi, ma)
+end
+
+"""
+    gensplits(mi, ma, cnt)
+
+Generate `cnt` float values uniformly between `mi` and `ma`.
+"""
+function uniform(mi, ma, cnt)
+    out = rand(cnt)
+    @assert(ma >= mi)
+    diff = ma - mi
+    out = out * diff .+ mi
+    return out
+end
+
+"""
+    findsplit!(n, data[, splitval_cnt])
+
+Find the best feature and its val for the given `node`.
+
+If `splitval_cnt` is unspecified, value of 10 is used.
+"""
+function findsplit!(n::Node, data, splitval_cnt::Integer=10)
+    bestscore, bestftr, bestval = Inf, -1, 0.0
+    for ftr = 1:length(data[1])
+        mi, ma = getextremas(ftr, n, data)
+        for splitval = uniform(mi, ma, splitval_cnt)
+            candidatescore = evaluatesplit(ftr, splitval, n, data)
+            if candidatescore < bestscore
+                bestscore = candidatescore
+                bestftr = ftr
+                bestval = splitval
+            end
+        end
+    end
+    @assert(bestftr != -1)
+    n.ftr = bestftr
+    n.splitval = bestval
+end
+
+"""
+    build!(parent, data)
+
+Recursively build tree from node.
+
+In the first call `parent` is likely the root of the tree.
+"""
+function build!(parent::Node, data)
+    if stopdividing(parent)
+        parent.isleaf = true
+        setprediction!(parent, data)
+    else
+        findsplit!(parent, data)
+        parent.left, parent.right = splitnode(parent.ftr, parent.splitval, 
+                                              parent)
+        build!(parent.left, data)
+        build!(parent.right, data)
+    end
+end
+
+"""
+    buildtree!(tree)
+
+Given `tree` build it.
+
+`tree` should contain all specs needed to build it.
+Works by recreating the `tree.root` and building with `build!`.
+"""
+function buildtree!(tree)
+    tree.root = typeof(tree.root)(Vector(1:length(tree.data)))
+    build!(tree.root, tree.data)
+end
+
+"""
+    predict(datapoint, model)
+"""
+function predict(datapoint, tree)
+    predict(datapoint, tree.root)
+end
+
+function predict(datapoint, node::Node)
+    if node.isleaf
+        return node.pred
+    end
+    if datapoint[node.ftr] < node.splitval
+        return predict(datapoint, node.left)
+    else
+        return predict(datapoint, node.right)
+    end
+end
+
--- regressiontree.jl
+++ regressiontree.jl
@@ -1,64 +1,65 @@
 include("utils.jl")
 include("node.jl")
 
-mutable struct RegressionTree
-    root::Node
+mutable struct RegTree
+    root::RegNode
     data::Vector{Dato}
 end
 
-function set_prediction(n::Node)
-    sz = size(n.examples_indices)
-    out::Float64 = 0.0
+function setprediction(n::RegNode, data)
+    result::Float64 = 0.0
+    sz = length(n.examples_indices)
     for j = 1:sz
         i = n.examples_indices[j]
-        out += data[i].y
+        result += data[i].y
     end
-    out /= sz
-    n.pred = out
+    result/= sz
+    n.pred = result
 end
 
-function stop_dividing(n::Node)
-    if size(n.examples) < 3
+function stopdividing(n::Node)
+    if length(n.examples_indices) < 3
         return true
     end
     return false
 end
 
-function evaluate(left::Node, right::Node)
+function evaluate(left::RegNode, right::RegNode)
     return MSE(left) + MSE(right)
 end
 
 function MSE(n::Node)
-    sz = size(n.examples_indices)
+    sz = length(n.examples_indices)
     res::Float64 = 0.0
     for j = 1:sz
         i = n.examples_indices[j]
-        res += (n.pred - data[i])^2
+        res += (n.pred - data[i].y)^2
     end
     res /= sz
     return res
 end
 
-function split_node(ftr::Int64, val::Float64, n::Node)
-    left = Node()
-    right = Node()
-    ex_sz = size(n.examples_indices)
+function splitnode(ftr, val, n::Node)
+    # Make sure children are the same type as parrent.
+    left = typeof(n)()
+    right = typeof(n)()
+    ex_sz = length(n.examples_indices)
     for j = 1:ex_sz
-        if data[n.example_indices[j]][ftr] < val
-            push!(left.example_indices, n.example_indices[j])
+        if data[n.examples_indices[j]][ftr] < val
+            push!(left.examples_indices, n.examples_indices[j])
         else
-            push!(right.example_indices, n.example_indices[j])
+            push!(right.examples_indices, n.examples_indices[j])
         end
     end
     return (left, right)
 end
 
-function evaluate_split(ftr::Int64, val::Float64, n::Node)
-    left, right = split_node(ftr, val, n)
-    return evalueate(left, right)
+function evaluatesplit(ftr::Int64, val::Float64, n::Node)
+    left, right = splitnode(ftr, val, n)
+    return evaluate(left, right)
 end
 
-function get_extremas(ftr::Int64, ex_sz::Int64, n::Node)
+function getextremas(ftr::Int64, ex_sz::Int64, n::Node)
     mi::Float64 = Inf
     ma::Float64 = -Inf
     for j = 1:ex_sz
@@ -71,43 +72,61 @@ function get_extremas(ftr::Int64, ex_sz::Int64, n::Node)
     return (mi, ma)
 end
 
-function best_split(n::Node)
-    ftr_sz = size(data[1])
-    ex_sz = size(n.examples_indices)
-    best_score = Inf
-    best_ftr = -1
-    best_val = 0.0
+function findsplit!(n::Node)
+    ftr_sz = length(data[1])
+    ex_sz = length(n.examples_indices)
+    bestscore = Inf
+    bestftr = -1
+    bestval = 0.0
     for ftr = 1:ftr_sz
-        mi, ma = get_extremas(ftr, ex_sz, n)
+        mi, ma = getextremas(ftr, ex_sz, n)
         # Later iterate over more split vals and test different ways to get
         # vals
-        split_val = (ma + mi) / 2.0
-        score_candidate = evaluate_split(ftr, val, n)
+        splitval = (ma + mi) / 2.0
+        score_candidate = evaluatesplit(ftr, splitval, n)
         # Nelze se nějak vyhýbat floatům? Vždyť se tu to v nich topí.
-        if score_candidate < best_score
-            best_score = score_candidate
-            best_ftr = ftr
-            best_val = split_val
+        if score_candidate < bestscore
+            bestscore = score_candidate
+            bestftr = ftr
+            bestval = splitval
         end
     end
-    @assert(best_ftr != -1)
-    return split_node(best_ftr, best_val, n)
+    @assert(bestftr != -1)
+    n.ftr = bestftr
+    n.splitval = bestval
 end
 
-function build(parent::Node)
-    if stop_dividing(parent)
-        parent.is_leaf = true
-        set_prediction(parent)
+function build(parent::Node, data)
+    if stopdividing(parent)
+        parent.isleaf = true
+        setprediction(parent, data)
     else
-        parent.left, parent.right = best_split(parent)
-        build(parent.left)
-        build(parent.right)
+        findsplit!(parent)
+        parent.left, parent.right = splitnode(parent.ftr, parent.splitval, 
+                                              parent)
+        build(parent.left, data)
+        build(parent.right, data)
     end
 end
 
-function build_tree()
-    root = Node(Vector(1:size(data)))
-    build(root)
+function buildtree!(tree)
+    tree.root = typeof(tree.root)(Vector(1:length(tree.data)))
+    build(tree.root, tree.data)
+end
+
+function predict(datapoint, node::Node)
+    if node.isleaf
+        return node.pred
+    end
+    if datapoint[node.ftr] < node.splitval
+        return predict(datapoint, node.left)
+    else
+        return predict(datapoint, node.right)
+    end
+end
+
+function predict(datapoint, tree)
+    predict(datapoint, tree.root)
 end
 
 data = [
@@ -116,4 +135,6 @@ data = [
         Dato([1,0,2], 1)
         Dato([10,2,3], 0)
        ]
-#rt = RegressionTree(Node(), data)
+
+rt = RegTree(RegNode(), data)
+buildtree!(rt)

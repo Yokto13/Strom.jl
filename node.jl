1b01cf13ae9f00abe5d05ad909b72a6382e10357
Yokto13 <nikousf@seznam.cz>
2022-08-15 15:58:52 +0200

    no split to empty node and custom stop cond

--- node.jl
+++ node.jl
@@ -1,6 +1,12 @@
+using Random
+
+rng_seed = 42
+Random.seed!(rng_seed)
+
 include("utils.jl")
 
 abstract type Node end
+abstract type Tree end
 
 mutable struct RegNode <: Node
     isleaf::Bool
@@ -10,10 +16,11 @@ mutable struct RegNode <: Node
     ftr::Integer
     left::Union{Nothing, Node}
     right::Union{Nothing, Node}
+    depth::Integer
 end
 
-RegNode(v::Vector{Int64}) = RegNode(false, -1, v, -1, -1, nothing, nothing)
-RegNode() = RegNode(false, -1, [], -1, -1, nothing, nothing)
+RegNode(v::Vector{Int64}) = RegNode(false, -1, v, -1, -1, nothing, nothing, 1)
+RegNode() = RegNode(false, -1, [], -1, -1, nothing, nothing, 1)
 
 mutable struct ClsNode <: Node
     isleaf::Bool
@@ -23,17 +30,21 @@ mutable struct ClsNode <: Node
     ftr::Integer
     left::Union{Nothing, Node}
     right::Union{Nothing, Node}
+    depth::Integer
 end
 
-ClsNode(v::Vector{Int64}) = ClsNode(false, -1, v, -1, -1, nothing, nothing)
-ClsNode() = ClsNode(false, -1, [], -1, -1, nothing, nothing)
+ClsNode(v::Vector{Int64}) = ClsNode(false, -1, v, -1, -1, nothing, nothing, 1)
+ClsNode() = ClsNode(false, -1, [], -1, -1, nothing, nothing, 1)
 
-mutable struct RegTree
+mutable struct RegTree <: Tree
     data::Vector{Dato}
     root::RegNode
+    minnode
+    maxdepth
 end
 
-RegTree(data::Vector{Dato}) = RegTree(data, RegNode())
+RegTree(data) = RegTree(data, RegNode(), 2, nothing)
+RegTree(data, minnode, maxdepth) = RegTree(data, RegNode(), minnode, maxdepth)
 
 """
     setprediction!(n, data)
@@ -58,13 +69,41 @@ Determine if node dividing should halt.
 
 If `cond` is unspecified, apply x -> length(x.datainds) < 2.
 """
-function stopdividing(n::Node, cond::Function= x -> length(x.datainds) < 2)
+function stopdividing(n::Node,
+        cond::Union{Function, Bool}= x -> length(x.datainds) < 2)
     if cond(n)
         return true
     end
     return false
 end
 
+"""
+    stopdividing(n, tree)
+"""
+function stopdividing(n::Node, tree::Tree)
+    cond = get_stopcondition(tree)
+    return stopdividing(n, cond)
+end
+
+"""
+    get_stopcondition(tree)
+
+Creates partial function from `tree` which can determine node split.
+"""
+function get_stopcondition(tree::Tree)
+    return x -> ((!isnothing(tree.maxdepth) && n.depth >= tree.maxdepth) 
+                 || (!isnothing(tree.minnode) && length(n.datainds) < tree.minnode)
+                )
+end
+
+"""
+    evaluate(n)
+
+Evaluate node.
+"""
+function evaluate(n::RegNode)
+    return MSE(n)
+end
 """
     evaluate(left, right)
 
@@ -73,7 +112,7 @@ Calculate error for left and right and get its sum.
 Used to score how good a certain node split is.
 """
 function evaluate(left::RegNode, right::RegNode)
-    return MSE(left) + MSE(right)
+    return evaluate(left) + evaluate(right)
 end
 
 function MSE(n::Node)
@@ -83,20 +122,23 @@ function MSE(n::Node)
         i = n.datainds[j]
         res += (n.pred - data[i].y)^2
     end
-    res /= sz
+    if sz != 0
+        res /= sz
+    end
     return res
 end
 
 """
-    splitnode(ftr, val, node)
+    splitnode(ftr, val, node, data)
 
 Get child-nodes containing `datainds` split on feature `ftr` and value `val`.
 
 All points < `val` are placed to the `left`, the rest to the `right`. 
 """
-function splitnode(ftr, val, n::Node)
+function splitnode(ftr, val, n::Node, data)
     left, right = typeof(n)(), typeof(n)()
     for j = 1:length(n.datainds)
+        #println(j, " ", ftr)
         if data[n.datainds[j]][ftr] < val
             push!(left.datainds, n.datainds[j])
         else
@@ -110,7 +152,7 @@ end
     eva;iatesplit(ftr, val, n, data)
 """
 function evaluatesplit(ftr::Int64, val::Float64, n::Node, data)
-    left, right = splitnode(ftr, val, n)
+    left, right = splitnode(ftr, val, n, data)
     setprediction!(left, data)
     setprediction!(right, data)
     return evaluate(left, right)
@@ -139,6 +181,9 @@ Find min and max of `ftr` in `n.datainds`.
 function getextremas(ftr::Int64, n::Node, data)
     mi::Float64 = Inf
     ma::Float64 = -Inf
+    #println("ftr ",ftr) 
+    #println(data)
+    #println(typeof(data))
     for j = 1:length(n.datainds)
         ex_i = n.datainds[j]
         mi = min(mi, data[ex_i][ftr])
@@ -167,13 +212,16 @@ end
 
 Find the best feature and its val for the given `node`.
 
-If `splitval_cnt` is unspecified, value of 10 is used.
+If `splitval_cnt` is unspecified, value of 50 is used.
 """
-function findsplit!(n::Node, data, splitval_cnt::Integer=10)
-    bestscore, bestftr, bestval = Inf, -1, 0.0
+function findsplit!(n::Node, data, splitval_cnt::Integer=50)
+    setprediction!(n, data)
+    bestscore = evaluate(n)
+    bestftr, bestval = -2, 0.0
     for ftr = 1:length(data[1])
         mi, ma = getextremas(ftr, n, data)
         for splitval = uniform(mi, ma, splitval_cnt)
+            splitval = (ma + mi) / 2
             candidatescore = evaluatesplit(ftr, splitval, n, data)
             if candidatescore < bestscore
                 bestscore = candidatescore
@@ -182,28 +230,38 @@ function findsplit!(n::Node, data, splitval_cnt::Integer=10)
             end
         end
     end
-    @assert(bestftr != -1)
     n.ftr = bestftr
     n.splitval = bestval
 end
 
 """
-    build!(parent, data)
+    build!(parent, tree)
 
 Recursively build tree from node.
 
 In the first call `parent` is likely the root of the tree.
+`tree` is passed because it holds attributes modifiing how should the build
+proceed. 
+Theoretically `parent` could belong to a different tree and `tree` would serve
+as a recipe defining how should the build proceed.
+However this is descouraged.
 """
-function build!(parent::Node, data)
-    if stopdividing(parent)
+function build!(parent::Node, tree)
+    data = tree.data
+    if stopdividing(parent, tree)
         parent.isleaf = true
         setprediction!(parent, data)
     else
         findsplit!(parent, data)
-        parent.left, parent.right = splitnode(parent.ftr, parent.splitval, 
-                                              parent)
-        build!(parent.left, data)
-        build!(parent.right, data)
+        if parent.ftr != -2
+            parent.left, parent.right = splitnode(parent.ftr, parent.splitval, 
+                                                parent, data)
+            build!(parent.left, tree)
+            build!(parent.right, tree)
+        else
+            parent.ftr = -1
+            parent.isleaf = true
+        end
     end
 end
 
@@ -217,7 +275,7 @@ Works by recreating the `tree.root` and building with `build!`.
 """
 function buildtree!(tree)
     tree.root = typeof(tree.root)(Vector(1:length(tree.data)))
-    build!(tree.root, tree.data)
+    build!(tree.root, tree)
 end
 
 """

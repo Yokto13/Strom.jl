c513cc0e9bfa8a3d84da891c15a27fde66cce742
Dominik Farhan <nikousf@seznam.cz>
2022-08-24 12:49:34 +0200

    fix svd add gauss-jordan

--- pca.jl
+++ pca.jl
@@ -1,14 +1,91 @@
-""" 
-Functions in this feel were written for author's enjoynment. 
-They are not optimized and their tests are very basic.
-Most of them are implemented in LinearAlgebra package and this 
-package should be used in real applications instead of this.
+# Functions in this feel were written for author's enjoynment. 
+# They are not optimized and their tests are very basic.
+# Most of them are implemented in LinearAlgebra package and this 
+# package should be used in real applications instead of this.
+# --arklez
 
---arklez
-"""
 using LinearAlgebra
 
-function eig_vals(A, iters=100)
+function null(A, l, i, eps=1e-7)
+    for k=i:size(A)[1]
+        if abs(A[k, l]) >= eps
+            return false
+        end
+    end
+    return true
+end
+
+function gauss_jordan(A, eps=1e-7)
+    i, j = 1, 1
+    m, n = size(A)
+    while true
+        kill = true
+        for k=i:m
+            for l=j:n
+                kill = kill & (abs(A[k, l]) < eps)
+            end
+        end
+        if kill
+            return A
+        end
+        while j <= n && null(A, j, i)
+            j += 1
+        end
+        k = i
+        while abs(A[k, j]) < eps
+            k += 1
+        end
+        if k != i
+            B = A'
+            r = B[:, k]
+            B[:, k] = B[:, i]
+            B[:, i] = r
+            A = B'
+        end
+        if abs(A[i, j] - 1) >= eps
+            for k=j+1:n
+                A[i, k] /= A[i, j]
+            end
+            A[i, k] = 1
+        end
+        for k=1:m
+            if k == i && continue; end
+            ratio = A[k, j] / A[i, j]
+            for l=j:n
+                A[k, l] = A[k, l] - ratio * A[i, l]
+            end
+        end
+        i += 1; j += 1
+        end
+    return A    
+end
+
+function read_solution(U, eps=1e-7)
+    m, n = size(U)
+    sol = zeros(n)
+    for i=m:-1:1
+        start = nothing
+        for j=1:n
+            if isnothing(start) && abs(U[i, j]) >= eps
+                start = j
+            else 
+                if !isnothing(start) && sol[j] == 0.0 && abs(U[i, j]) >= eps
+                    sol[j] = 1
+                end
+            end
+        end
+        if !isnothing(start)
+            tot = 0
+            for j=(start + 1):n
+                tot += sol[j] * U[i, j]
+            end
+            sol[start] = - tot / U[i, start]
+        end
+    end
+    return sol
+end
+
+function eig_vals(A, iters=1000)
     for i=1:iters
         Q, R = qr(A)
         A = R * Q
@@ -17,45 +94,86 @@ function eig_vals(A, iters=100)
 end
 
 function IPM(A, μ, iters, shift=1e-7)
-    D = size(A)[1]
-    b = ones(D)
+    D = size(A)[2]
+    b = rand(D)
+    b /= norm(b)
     μ += shift
     inverse = inv(A - μ * I(D))
-    C = norm(inverse * b)
     for i=1:iters
-        b = (inverse * b) / C
-        C = norm(inverse * b)
+        b = (inverse * b)
+        b /= norm(b)
     end
     return b 
 end
 
-function eig_vectors(A, λs, iters=50)
-    result = []
+function eig_vectors(A, λs, iters=10000)
+    vectors = Dict()
     for λ=λs
-        push!(result, IPM(A, λ, iters))
+        if λ in keys(vectors)
+            M = zeros(length(vectors[λ][1]), length(vectors[λ]))
+            for i=1:length(vectors[λ])
+                M[:, i] = vectors[λ][i]
+            end
+            M = M'
+            U = gauss_jordan(M)
+            perp = read_solution(U)
+            perp /= norm(perp)
+            push!(vectors[λ], perp)
+        else
+            vectors[λ] = [IPM(A, λ, iters)]
+        end
+    end
+    out = []
+    used_λs = Set()
+    for λ in λs
+        if λ in used_λs && continue; end
+        for v in vectors[λ]
+            push!(out, v)
+        end
+        push!(used_λs, λ)
+    end
+    return out
+end
+
+function get_2nd_base(A, σs, vs, eps=1e-7)
+    result = zeros(size(A)[1], size(A)[1])
+    println(σs)
+    println(size(result))
+    for i=1:length(σs)
+        start = 1 + (i - 1) * size(A)[1]
+        if abs(σs[i]) >= eps
+            #println("haha ",  A * vs[i] / σs[i])
+            #println(result)
+            result[start:start + size(A)[1] - 1] = A * vs[i] / σs[i]
+            #println(result)
+        else
+            #println("?????")
+            U = gauss_jordan(copy(result'))
+            #println(result')
+            #println(U)
+            perp = read_solution(U)
+            perp /= norm(perp)
+            #println("perp ", perp)
+            #println(result)
+            result[start:start + size(A)[1] - 1] = perp
+        end
     end
     return result
 end
 
-function get_2nd_base(A, σs, vs)
-    result = zeros(length(vs), length(vs[1]))
-    for i=1:length(vs)
-        start = 1 + (i - 1) * length(vs[1])
-        result[start:start + length(vs[1]) - 1] = A * vs[i] / σs[i]
-    end
-    return result
-end
 
-
-function SVD(A)
-    M = transpose(A) * A
+function SVD(A, eps=1e-7)
+    M = A' * A
+    m, n = size(A)
     λs = eig_vals(M)
     sort!(λs, rev=true)
     σs = sqrt.(λs)
-    V = eig_vectors(M, λs)
+    σs = [σ for σ in σs if abs(σ) >= eps]
+    V = eig_vectors(M, λs)[1:end .<= n, 1:end .<= n]
+    println("VVV  ", V)
     U = get_2nd_base(A, σs, V)
-    Σ = 1.0 * I(length(λs))
-    for i=1:length(λs)
+    Σ = zeros(m, n)
+    for i=1:length(σs)
         Σ[i, i] = σs[i] 
     end
     V = mapreduce(permutedims, vcat, V)
--- test/testpca.jl
+++ test/testpca.jl
@@ -1,6 +1,7 @@
 include("../pca.jl")
 
 using Test
+eps = 1e-7
 
 function vecand(v)
     prod = true
@@ -37,22 +38,44 @@ end
     A = [1 0; 0 0]
     dp = deepcopy(A)
     μ = 1.0
-    eps = 1e-7
     eps2 = 1e-3
     @test vecand(IPM(A, μ, 50) .- eps - [1, 0] .< [0, 0])
     @test dp == A
     A = [0 0; 0 1]
     dp = deepcopy(A)
     μ = 1.0
-    eps = 1e-7
     @test vecand(IPM(A, μ, 50) .- eps - [0, 1] .< [0, 0])
     @test dp == A
     A = [1 1; 1 1]
     dp = deepcopy(A)
     μ = 2.0
-    eps = 1e-7
     @test vecand(abs.(IPM(A, μ, 50)) .- eps - [0.707107, 0.707107] .< [0, 0])
     @test dp == A
+end
+
+function test_svdproperties(A)
+    U, Σ, V = SVD(A)
+    println("U ",U)
+    println("Si ",Σ)
+    println("V ", V)
+    σs = []
+    for i=1:size(Σ)[1]
+        if i > size(Σ)[2]
+            break
+        end
+        push!(σs, Σ[i, i])
+        @test Σ[i, i] + eps >= 0
+    end
+    @test σs == sort(σs, rev=true)
+    @test abs(norm(V[:, 1]) - 1) < eps
+    if size(V)[2] > 1
+        @test abs(V[:, 1]' * V[:, 2]) < eps
+    end
+    @test abs(norm(U[:, 1]) - 1) < eps
+    if size(U)[2] > 1
+        @test abs(U[:, 1]' * U[:, 2]) < eps
+    end
+    @test abs((U * Σ * V)[1] - A[1]) < eps 
 end
 
 @testset "SVD()" begin
@@ -62,4 +85,20 @@ end
     eps = 1e-4
     @test vecand(Σ - [1.618 0; 0 0.618] .- eps .< [ 0 0 ; 0 0])
     @test dp == A
+    test_svdproperties(A)
+    A = [1 0; 0 1]
+    test_svdproperties(A)
+    A = [1 1; 1 1]
+    test_svdproperties(A)
+    A = [1 1 2; 3 1 1; 0 0 10]
+    test_svdproperties(A)
+    A = [1 1 ; 3 1 ; 0 0 ]
+    test_svdproperties(A)
+    test_svdproperties(A')
+end
+
+@testset "PCA()" begin
+    A = [1 1; 0 1]
+    @test_throws ArgumentError PCA(A)
+    @test_throws ArgumentError PCA(A, [0 0; 0 0], 2)
 end
